09ed3eedf4a9b03aec2e5c28f0cd636800fdc7a1
Jeff.A <kinesthesiac@gmail.com>
2019-12-19 13:43:27 -0700

    basic page layout and a bit of css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,23 @@
 var text = document.getElementById("test");
 text.innerHTML = "beepbloop";
+
+
+// ask user for input
+
+// when input is recieved, start the game
+
+// computer picks a word randomly
+//computer displays blanks equal to the number of letters in the word
+//computer listens for letters to be pressed
+
+
+//computer checks to see if any more letters neeed to be guessed?
+//when a letter is pressed:
+//computer checks to see if it matches a letter in the word
+//computer prevents that letter from being used again
+//if it does match:
+//the matching blank is converted to display that letter
+//score goes up by one
+//if it doesn't match:
+//the letter pressed is displayed in the guessed letters
+//number of guesses is reduced by 1
